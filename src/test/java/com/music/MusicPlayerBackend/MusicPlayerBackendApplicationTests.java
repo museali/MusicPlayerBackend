5e270e9bfe749e8b9ba83b3b33fb4f6ebd9cea97
museali <museali_@hotmail.co.uk>
2023-08-10 19:34:23 +0100

    Building Project

---
+++ src/test/java/com/music/MusicPlayerBackend/MusicPlayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.MusicPlayerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
